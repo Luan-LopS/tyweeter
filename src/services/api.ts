a8def32a6fe50313076588a6dffb799f1926ecee
Luan-LopS <luam10@hotmail.com.br>
2026-02-24 11:37:35 -0300

    correcao de url

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,7 @@ import { getAuthHeaders } from './autHeaders'
 export const Api = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://127.0.0.1:8000/api/v1/',
+        baseUrl: 'https://luanlops.pythonanywhere.com/api/v1/',
         prepareHeaders: getAuthHeaders
     }),
     tagTypes: ['User'],
